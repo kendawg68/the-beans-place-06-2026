eb25721c3401b3886875d01799ae34806332390c
Kendrick Aragon <kendawg1996@gmail.comecho>
2026-06-17 20:00:33 -0500

    WIP Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,3 +54,31 @@
 // for the navigation links in the NavBar.
 
 /* --- YOUR COMPONENT CODE GOES HERE --- */
+import HeroSection from "./components/HeroSection";
+
+export default function App() {
+    return(
+        <div className="app">
+            {/* NAVBAR */}
+            {/* HERO */}
+            <section className="hero bg-hero">
+                <div className="hero-grid">
+                    <HeroSection />
+                </div>
+            </section>
+
+            {/* Features / Carousel */}
+
+            {/* Product Showcase */}
+
+            {/* CTA */}
+
+            {/* About */}
+
+            {/* Contact */}
+
+            {/* Footers */}
+            
+        </div>
+    );
+}
